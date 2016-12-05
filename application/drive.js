2c1440db033c5d1e8e8cc03d68e16a0a30a2084c
paulchu756 <lordbabypanda@gmail.com>
2016-12-05 21:27:57 +0000

    add project2 files in

---
+++ application/drive.js
@@ -0,0 +1,156 @@
+// populate people/states, also person/visit form submit
+$(document).ready(function(){
+	populatePeople();
+	populateStates();
+	displayData();
+
+	$('#addPersonSubmit').click(function(e){
+		e.preventDefault();
+		addPerson();
+	});
+
+	$('#addVisitSubmit').click(function(e){
+		e.preventDefault();
+		addVisit();
+	});
+});
+
+//display selected person
+function displayData()
+{
+	$("#SelectHumanDropDown").change(function(){
+		$.ajax({
+			type: "GET",
+			url: "api/visits",
+			dataType: "json",
+			success: function(data)
+			{
+				var i = $("#SelectHumanDropDown").val();
+				$("#displayInfo").empty();
+				
+				var firstName = data[i]["firstname"];
+				var lastName = data[i]["lastname"];
+				var food = data[i]["food"];
+				var stateAbb = data[i]["stateabb"];
+				var stateName = data[i]["statename"];
+				var dateVisit = data[i]["date_visited"]; 
+
+				$("#displayInfo").append(
+				"First name: " + firstName + 
+				"<br> Last name: " + lastName + 
+				"<br> Favorite food: " + food +
+				"<br> Visited : " + stateAbb + " " + stateName + 
+				"<br> on " + dateVisit);
+			}
+		});
+	});
+}
+
+//populate people's dropdowns
+function populatePeople()
+{
+	$.ajax({
+		type:"GET",
+		url:"api/people",
+		dataType:"json",
+		success : function(data)
+		{
+			//console.log('success');
+			//console.log(data);
+			$("#SelectHumanDropDown").empty();
+			$("#humanNameDropDown").empty();
+			var len = data.length;
+			for(var i = 0; i < len; i++)
+			{
+				var id = data[i]["id"];
+				var firstname = data[i]["firstname"];
+				$("#SelectHumanDropDown").append("<option value='" + id + "'>" + firstname + "</option>");
+				$("#humanNameDropDown").append("<option value='" + id + "'>" + firstname + "</option>");
+			}
+		},
+		error : function(data)
+		{
+			console.log('failed');
+			console.log(data);
+		}
+	});
+}
+
+//populate state dropdown
+function populateStates()
+{
+	$.ajax({
+		type:"GET",
+		url:"api/states",
+		dataType:"json",
+		success : function(data)
+		{
+			//console.log('success');
+			//console.log(data);
+			$("#stateNameDropDown").empty();
+			var len = data.length;
+			for(var i = 0; i < len; i++)
+			{
+				var id = data[i]["id"];
+				var stateName = data[i]["statename"];
+				$("#stateNameDropDown").append("<option value='" + id + "'>" + stateName + "</option>");
+			}
+		},
+		error : function(data)
+		{
+			console.log('failed');
+			console.log(data);
+		}
+	});
+}
+
+//Add person to database
+function addPerson()
+{
+	$.ajax({
+		type: "POST",
+		url: "api/people", // api/people
+		data: $("#personForm").serialize(),
+		 success: function(data,status,xhr) 
+		{
+			console.log(data);
+			console.log(status);
+			console.log(xhr);
+			console.log($("#personForm").serialize());
+			console.log("You have added a person");
+			populatePeople();
+			displayData();
+		},
+		 error: function(data,status,xhr) 
+		{
+			console.log(data);
+			console.log(status);
+			console.log(xhr);
+			console.log($("#personForm").serialize());
+			console.log("error");
+			populatePeople();
+			displayData();
+		}
+	});	
+}
+
+//Add visit to database
+function addVisit()
+{
+	$.ajax({
+		type: "POST",
+		url: "api/visits", // api/visits
+		data: $("#humanNameDropDown, #stateNameDropDown, #visitForm").serialize(),
+		success: function(data)
+		{
+			console.log(data);
+			console.log($("#visitForm").serialize());
+			console.log("You have added a visit");
+		},
+		error: function(data)
+		{
+			console.log(data);
+			console.log($("#visitForm").serialize());
+		}
+	});
+}
