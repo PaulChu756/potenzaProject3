e5ce8882005c47ebcdc494100e0dd98f3d4062e7
paulchu756 <lordbabypanda@gmail.com>
2016-12-06 22:20:25 +0000

    added controllers to module

---
+++ application/modules/api/controllers/PeopleController.php
@@ -0,0 +1,19 @@
+<?php
+
+class Api_PeopleController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+        echo "people controller";
+    }
+
+
+}
+
---
+++ application/modules/api/controllers/StatesController.php
@@ -0,0 +1,19 @@
+<?php
+
+class Api_StatesController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+        echo "states controller";
+    }
+
+
+}
+
